6f7a757aa45f371605f90138f44ef6d77cee8b95
Masum <masumt999.com>
2023-05-20 21:13:03 +0600

    delete data from my toy page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,6 +38,15 @@ async function run() {
       const result = await toyCollection.insertOne(newToy);
       res.send(result);
   });
+
+
+  // data delete
+  app.delete('/toy/:id', async(req, res) => {
+    const id  = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await toyCollection.deleteOne(query);
+    res.send(result); 
+  })
 
 
 // receive all data from database
